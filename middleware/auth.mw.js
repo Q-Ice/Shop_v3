563d14c3146fbc23d8a940b1219d2a818e4f5c37
绿叶 <78372813+Q-Ice@users.noreply.github.com>
2022-05-15 17:04:33 +0700

    Add files via upload

---
+++ middleware/auth.mw.js
@@ -0,0 +1,16 @@
+var connection = require('../models/product.models')
+
+module.exports.checkLogin = function (req, res, next) {
+    if (req.cookies.user) {
+        var sql = `SELECT * FROM customers WHERE customerId = '${req.cookies.user}'`
+        connection.query(sql, function (err, result) {
+            res.locals.userId = req.cookies.user
+            res.locals.userName = result[0].customerName
+        })
+    }
+    var sql2 = `SELECT COUNT(isSold) FROM products WHERE isSold = 'true'`
+    connection.query(sql2, function(err, result) {
+        res.locals.quantity = result[0][`COUNT(isSold)`]
+    })
+    next()
+}
